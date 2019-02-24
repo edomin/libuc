95ea46299f354ab47c0dc24a6637671dfeaab3e3
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2019-02-25 03:13:23 +0600

    Multiple fixes in test UC_Ucs4ToUtf8

--- test/UC_Ucs4ToUtf8.c
+++ test/UC_Ucs4ToUtf8.c
@@ -3,65 +3,86 @@
 #include <stdarg.h>
 #include <cmocka.h>
 
-void UC_UC_Ucs4ToUtf8_U32z_U8zSize1(void **state) {
+void UC_Ucs4ToUtf8_U32z_U8zSize1(void **state) {
     uint32_t u32char = 0x0000007Au; /* z */
-    uint8_t  u8char[5] = {'\0', '\0', '\0', '\0', '\0'};
+    uint8_t *u8char = UC_CreateUtf8();
     size_t   size = 0xDEADBEEFu;
 
-    UC_Ucs4ToUtf8(u32char, u8char, size);
+    UC_Ucs4ToUtf8(u32char, (uint8_t **)&u8char, &size);
 
     assert_memory_equal(u8char, "\x7A\0\0\0\0", 5u);
-    assert_true(nbytes == 1u);
+    assert_true(size == 1u);
 }
 
-void UC_UC_Ucs4ToUtf8_U32cyrillicYu_U8cyrillicYuSize2(void **state) {
+void UC_Ucs4ToUtf8_U32cyrillicYu_U8cyrillicYuSize2(void **state) {
     uint32_t u32char = 0x0000044Eu; /* ю */
-    uint8_t  u8char[5] = {'\0', '\0', '\0', '\0', '\0'};
+    uint8_t *u8char = UC_CreateUtf8();
     size_t   size = 0xDEADBEEFu;
 
-    UC_Ucs4ToUtf8(u32char, u8char, size);
+    UC_Ucs4ToUtf8(u32char, (uint8_t **)&u8char, &size);
 
     assert_memory_equal(u8char, "\xD1\x8E\0\0\0", 5u);
-    assert_true(nbytes == 2u);
+    assert_true(size == 2u);
 }
 
-void UC_UC_Ucs4ToUtf8_U32chinesseTiger_U8chinesseTigerSize3(void **state) {
+void UC_Ucs4ToUtf8_U32chinesseTiger_U8chinesseTigerSize3(void **state) {
     uint32_t u32char = 0x0000864Eu; /* 虎 */
-    uint8_t  u8char[5] = {'\0', '\0', '\0', '\0', '\0'};
+    uint8_t *u8char = UC_CreateUtf8();
     size_t   size = 0xDEADBEEFu;
 
-    UC_Ucs4ToUtf8(u32char, u8char, size);
+    UC_Ucs4ToUtf8(u32char, (uint8_t **)&u8char, &size);
 
     assert_memory_equal(u8char, "\xE8\x99\x8E\0\0", 5u);
-    assert_true(nbytes == 3u);
+    assert_true(size == 3u);
 }
 
-void UC_UC_Ucs4ToUtf8_U32tooBig_U8spaceSize1(void **state) {
+void UC_Ucs4ToUtf8_U32tooBig_U8spaceSize1(void **state) {
     uint32_t u32char = 0x10000000u;
-    uint8_t  u8char[5] = {'\0', '\0', '\0', '\0', '\0'};
+    uint8_t *u8char = UC_CreateUtf8();
     size_t   size = 0xDEADBEEFu;
 
-    UC_Ucs4ToUtf8(u32char, u8char, size);
+    UC_Ucs4ToUtf8(u32char, (uint8_t **)&u8char, &size);
 
     assert_memory_equal(u8char, "\x20\0\0\0\0", 5u);
-    assert_true(nbytes == 1u);
+    assert_true(size == 1u);
 }
 
-void UC_UC_Ucs4ToUtf8_U32zToNull_NothingSize0(void **state) {
+void UC_Ucs4ToUtf8_U32zToPointerToNull_NothingSize0(void **state) {
+    uint32_t u32char = 0x0000007Au; /* z */
+    uint8_t *u8char = NULL;
+    size_t   size = 0xDEADBEEFu;
+
+    UC_Ucs4ToUtf8(u32char, (uint8_t **)&u8char, &size);
+
+    assert_null(u8char);
+    assert_true(size == 0u);
+}
+
+void UC_Ucs4ToUtf8_U32zToNull_NothingSize0(void **state) {
     uint32_t u32char = 0x0000007Au; /* z */
     size_t   size = 0xDEADBEEFu;
 
-    UC_Ucs4ToUtf8(u32char, NULL, size);
+    UC_Ucs4ToUtf8(u32char, NULL, &size);
 
-    assert_true(nbytes == 0u);
+    assert_true(size == 0u);
+}
+
+void UC_Ucs4ToUtf8_U32zSizeNull_U8zSizeNull(void **state) {
+    uint32_t u32char = 0x0000007Au; /* z */
+    uint8_t *u8char = UC_CreateUtf8();
+
+    UC_Ucs4ToUtf8(u32char, (uint8_t **)&u8char, NULL);
+
+    assert_memory_equal(u8char, "\x7A\0\0\0\0", 5u);
 }
 
 const struct CMUnitTest testsGroup[] = {
-    cmocka_unit_test(UC_UC_Ucs4ToUtf8_U32z_U8zSize1),
-    cmocka_unit_test(UC_UC_Ucs4ToUtf8_U32cyrillicYu_U8cyrillicYuSize2),
-    cmocka_unit_test(UC_UC_Ucs4ToUtf8_U32chinesseTiger_U8chinesseTigerSize3),
-    cmocka_unit_test(UC_UC_Ucs4ToUtf8_U32tooBig_U8spaceSize1),
-    cmocka_unit_test(UC_UC_Ucs4ToUtf8_U32zToNull_NothingSize0),
+    cmocka_unit_test(UC_Ucs4ToUtf8_U32z_U8zSize1),
+    cmocka_unit_test(UC_Ucs4ToUtf8_U32cyrillicYu_U8cyrillicYuSize2),
+    cmocka_unit_test(UC_Ucs4ToUtf8_U32chinesseTiger_U8chinesseTigerSize3),
+    cmocka_unit_test(UC_Ucs4ToUtf8_U32tooBig_U8spaceSize1),
+    cmocka_unit_test(UC_Ucs4ToUtf8_U32zToPointerToNull_NothingSize0),
+    cmocka_unit_test(UC_Ucs4ToUtf8_U32zToNull_NothingSize0),
 };
 
 int main(void) {
