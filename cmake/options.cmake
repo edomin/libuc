f0fa29fda7f80c5479dab8a17cb21b24fc335156
Vasiliy Edomin <Vasiliy.Edomin@gmail.com>
2020-03-28 00:46:45 +0600

    Add build options UC_BUILD_DOC and UC_WERROR

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,2 +1,4 @@
-option(UC_MORE_WARNINGS "More compiler warnings" ON)
+option(UC_BUILD_DOC "Build documentation" OFF)
 option(UC_CODE_COVERAGE "Test coverage" OFF)
+option(UC_MORE_WARNINGS "More compiler warnings" OFF)
+option(UC_WERROR "Compile with -Werror option" OFF)
